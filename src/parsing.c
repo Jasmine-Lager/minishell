e1df344ab807ed7ab85de1a4f84c08f2f0fe67e7
sevcikk2 <katasevcikova20@seznam.cz>
2025-08-06 23:28:08 +0200

    made it compile

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ksevciko <ksevciko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 19:53:07 by ksevciko          #+#    #+#             */
-/*   Updated: 2025/08/06 23:22:28 by ksevciko         ###   ########.fr       */
+/*   Updated: 2025/08/06 23:27:46 by ksevciko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 void	find_start_end_of_token(t_mini *var, int *start_token, int *end_token, t_token *new) //needs to be modified a lot - should make |, <, >, <<, >> in a single token regardless of whether there are spaces around, also this logic for end_token does not work.
 {
-	char	*divider;
+	char	divider;
 
 	while (var->line[*start_token] == ' ')
-		*start_token++;
+		(*start_token)++;
 	new->quotes = NONE;
 	if (var->line[*start_token] == '"')
 	{
 		divider = '"';
 		new->quotes = DOUBLE;
-		*start_token++;
+		(*start_token)++;
 	}
 	else if (var->line[*start_token] == 39)
 	{
 		divider = 39;
 		new->quotes = SINGLE;
-		*start_token++;
+		(*start_token)++;
 	}
 	else
 		divider = ' ';
 	*end_token = *start_token + 1;
 	while (var->line[*end_token] && var->line[*end_token] != divider)
-		*end_token++;
+		(*end_token)++;
 	if (var->line[*end_token] == '\0' && divider != ' ')
 		other_error(var, "error: unclosed quotes\n");
 }
@@ -67,7 +67,7 @@ void	find_token_type(t_token *new, t_token *last) //maybe fill infile, outfile, 
 }
 
 void	create_one_token(t_mini *var, int *start_token, int *end_token,
-			t_tokens *last)
+			t_token *last)
 {
 	t_token	*new;
 
@@ -84,13 +84,13 @@ void	create_one_token(t_mini *var, int *start_token, int *end_token,
 	if (!new)
 		other_error(var, "malloc failled\n");
 	new->next = NULL;
-	divider = find_start_end_of_token(var, start_token, end_token, new);
+	find_start_end_of_token(var, start_token, end_token, new);
 	new->content = (char *)malloc((end_token - start_token + 1)
 		* sizeof(char));
 	if (!new->content)
 		other_error(var, "malloc failled\n");
-	ft_strlcpy(new->content, var->line[*start_token],
-		end_token - start_token + 1)
+	ft_strlcpy(new->content, &var->line[*start_token],
+		end_token - start_token + 1);
 	find_token_type(new, last);
 	*last = *new;
 }
@@ -99,13 +99,13 @@ void	parse(t_mini *var)
 {
 	int			start_token; //index of a start of a token in the line
 	int			end_token; //index one after the end of the token in the line
-	t_tokens	*last;
+	t_token		*last;
 
 	start_token = 0;
 	end_token = 0;
 	last = NULL;
 	while (var->line[start_token])
-		create_one_token(var, start_token, end_token, last);
+		create_one_token(var, &start_token, &end_token, last);
 
 	
 	if (!ft_strncmp("exit", var->line, 5))
