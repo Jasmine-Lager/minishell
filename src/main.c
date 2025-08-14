3037ec5bb63877ea48d763b9fb539ad53ef12631
Jasmine Lager <jlager@student.42prague.com>
2025-08-14 16:09:39 +0200

    slight main change

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jasminelager <jasminelager@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 16:00:52 by ksevciko          #+#    #+#             */
-/*   Updated: 2025/08/14 15:43:41 by jasminelage      ###   ########.fr       */
+/*   Updated: 2025/08/14 16:02:37 by jasminelage      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,29 @@
 
 volatile sig_atomic_t g_signal = 0;
 
-int	main(int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	t_mini	*var; // this I would maybe call input or something -but it is not just input, but all variables needed by the whole program (like pipes)
-		// like t_minishell *input - this is very long, and would make it harder to meet norminette rules, if this name would be everywhere in the project
-		//could be t_shell *info, or t_mini *struct maybe? i like t_mini *var though, and think it would be waste of time to rename it. do it if you think it would improve the project though, but change it to something short and change it everywhere
+	t_mini *var;
 
+	// extra edge case? would just about fit
+	// if (envp == NULL || *envp == NULL)
+	// 	return (printsdr(BOLD RED "No environment found. \nExiting..", RESET));
 	var = malloc(sizeof(t_mini));
-	initialize_minishell(var, argc, argv, envp); //no need to check return value, it does not return, on error it exits
-	// setup_signals();
-	// ;
+	initialize_minishell(var, argc, argv, envp);
+	signals_setup();
 	// REPL
 	// = Read > Evaluate > Print > Loop
 	while (1)
 	{
-		var->line = readline("$ ");
-		if (var->line == NULL) // Ctrl+D has been pressed to terminate the program
+		g_signal = 0;
+		var->line = readline(BOLD GRAY "$ " RESET);
+		// making the BOLD GRAY the defult color??
+		if (var->line == NULL)
+		// Ctrl+D has been pressed to terminate the program
 		{
-			ft_printf("exiting..\n");
+			ft_printf(BOLD GRAY "Exiting..\n" RESET);
 			free_var_exit(var, 0);
-		}	
+		}
 		if (*var->line)
 		{
 			add_history(var->line);
